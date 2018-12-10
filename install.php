64a42b547716855bcc697bb554a188f98b373fb2
Charlene Auger <charlene.auger@factorfx.com>
2018-12-10 16:17:48 +0100

    Update plugin for extension engine

--- install.php
+++ install.php
@@ -1,42 +1,42 @@
 <?php
-function plugin_version_driverslist()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_driverslist()
 {
-return array('name' => 'driverslist',
-'version' => '1.1',
-'author'=> 'Stephane PAUTREL',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `driverslist` (
+                              `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                              `HARDWARE_ID` INT(11) NOT NULL,
+                              `CLASS` VARCHAR(255) DEFAULT NULL,
+                              `DESCRIPTION` VARCHAR(255) DEFAULT NULL,
+                              `NAME` VARCHAR(255) DEFAULT NULL,
+                              `PROVIDERNAME` VARCHAR(255) DEFAULT NULL,
+                              `VERSION` VARCHAR(255) DEFAULT NULL,
+                              `DATE` VARCHAR(255) DEFAULT NULL,
+                              `INFNAME` VARCHAR(255) DEFAULT NULL,
+                              `FRIENDLYNAME` VARCHAR(255) DEFAULT NULL,
+                              `SIGNED` VARCHAR(255) DEFAULT NULL,
+                              `DEVICEID` VARCHAR(255) DEFAULT NULL,
+                              PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                            ) ENGINE=INNODB ;");
 }
 
-function plugin_init_driverslist()
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_driverslist()
 {
-$object = new plugins;
-$object -> add_cd_entry("driverslist","hardware");
-
-$object -> sql_query("CREATE TABLE IF NOT EXISTS `driverslist` (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `HARDWARE_ID` INT(11) NOT NULL,
-  `CLASS` VARCHAR(255) DEFAULT NULL,
-  `DESCRIPTION` VARCHAR(255) DEFAULT NULL,
-  `NAME` VARCHAR(255) DEFAULT NULL,
-  `PROVIDERNAME` VARCHAR(255) DEFAULT NULL,
-  `VERSION` VARCHAR(255) DEFAULT NULL,
-  `DATE` VARCHAR(255) DEFAULT NULL,
-  `INFNAME` VARCHAR(255) DEFAULT NULL,
-  `FRIENDLYNAME` VARCHAR(255) DEFAULT NULL,
-  `SIGNED` VARCHAR(255) DEFAULT NULL,
-  `DEVICEID` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY  (`ID`,`HARDWARE_ID`)
-) ENGINE=INNODB ;");
-
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE `driverslist`;");
 }
 
-function plugin_delete_driverslist()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_driverslist()
 {
-$object = new plugins;
-$object -> del_cd_entry("driverslist");
-$object -> sql_query("DROP TABLE `driverslist`;");
 
 }
-
-?>
